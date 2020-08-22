e5cf5a371c92b551a7e7f117497be00d047d5a21
Alex Wong <alex.wong@bcgdv.com>
2020-08-22 13:39:41 +0900

    chunk payload since only 10 elements can fit in a FIELD array

--- event-handlers/handle-PostPush.js
+++ event-handlers/handle-PostPush.js
@@ -8,48 +8,55 @@ async function run(context, args) {
   try {
     const statusPath = path.join(__dirname, "..", "log", "status.json")
     const resourceStatus = await asyncFSReadFile(statusPath, "utf-8")
-    let parsedResourceStatus
-    if (resourceStatus) {
-      parsedResourceStatus = JSON.parse(resourceStatus)
-    }
-    if (!parsedResourceStatus || parsedResourceStatus.length === 0) {
+    if (!resourceStatus) {
       return
-    }
-    const STS = new AWS.STS({
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_KEY,
-    })
-    const callerIdentity = await STS.getCallerIdentity().promise()
-    const callerIdentityArray = callerIdentity.Arn.split("/")
-    const username = callerIdentityArray[callerIdentityArray.length - 1]
-    const amplifyEnv = context.exeInfo.localEnvInfo.envName
-    const slackPayload = {
-      blocks: [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `${username} pushed to amplify env: *${amplifyEnv}*`,
+    } else {
+      parsedResourceStatus = JSON.parse(resourceStatus)
+      if (!parsedResourceStatus || parsedResourceStatus.length === 0) {
+        return
+      }
+      let chunkedResources = []
+      for (let i = 0; i < parsedResourceStatus.length; i += 10) {
+        chunkedResources.push(parsedResourceStatus.slice(i, i + 10))
+      }
+
+      const STS = new AWS.STS({
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+      })
+      const callerIdentity = await STS.getCallerIdentity().promise()
+      const callerIdentityArray = callerIdentity.Arn.split("/")
+      const username = callerIdentityArray[callerIdentityArray.length - 1]
+      const amplifyEnv = context.exeInfo.localEnvInfo.envName
+      const slackPayload = {
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: `${username} pushed to amplify env: *${amplifyEnv}*`,
+            },
           },
-        },
-        {
-          type: "divider",
-        },
-        {
+          {
+            type: "divider",
+          },
+        ],
+      }
+      for (let j = 0; j < chunkedResources.length; j++) {
+        slackPayload.blocks.push({
           type: "section",
-          fields: parsedResourceStatus,
-        },
-      ],
-    }
-    const command = `curl -X POST -H 'Content-type: application/json' --data '${JSON.stringify(
-      slackPayload
-    )}' ${process.env.AMPLIFY_SLACK_WEBHOOK_URL}`
-    const { stdout, stderr } = await exec(command)
-    if (stdout && stdout !== "ok") {
-      return context.print.error(`\nNotify Slack Error: ${stdout}`)
-    }
-    if (stderr) {
-      return context.print.error(`\nNotify Slack Error: ${stdout}`)
+          fields: chunkedResources[j],
+        })
+      }
+      const command = `curl -X POST -H 'Content-type: application/json' --data '${JSON.stringify(
+        slackPayload
+      )}' ${process.env.AMPLIFY_SLACK_WEBHOOK_URL}`
+      const { stdout, stderr } = await exec(command)
+      if (stdout && stdout === "ok") {
+        return
+      } else if (stderr && stdout) {
+        return context.print.error(`\nNotify Slack Error: ${stdout}`)
+      }
     }
   } catch (err) {
     context.print.error(`\nNotify Slack Error: ${err}`)
--- event-handlers/handle-PrePush.js
+++ event-handlers/handle-PrePush.js
@@ -1,48 +1,48 @@
-const eventName = 'PrePush'
-const util = require('util')
-const path = require('path')
+const eventName = "PrePush"
+const util = require("util")
+const path = require("path")
 
-const asyncFSWriteFile = util.promisify(require('fs').writeFile)
+const asyncFSWriteFile = util.promisify(require("fs").writeFile)
 async function run(context, args) {
   try {
     const {
       resourcesToBeCreated,
       resourcesToBeUpdated,
-      resourcesToBeDeleted
+      resourcesToBeDeleted,
     } = await context.amplify.getResourceStatus()
     const FIELDS = []
 
     for (let i = 0; i < resourcesToBeCreated.length; i++) {
       FIELDS.push({
-        type: 'plain_text',
-        text: `[created] ${resourcesToBeCreated[i].category}`
+        type: "plain_text",
+        text: `[created] ${resourcesToBeCreated[i].category}`,
       })
       FIELDS.push({
-        type: 'plain_text',
-        text: resourcesToBeCreated[i].resourceName
+        type: "plain_text",
+        text: resourcesToBeCreated[i].resourceName,
       })
     }
     for (let i = 0; i < resourcesToBeUpdated.length; i++) {
       FIELDS.push({
-        type: 'plain_text',
-        text: `[updated] ${resourcesToBeUpdated[i].category}`
+        type: "plain_text",
+        text: `[updated] ${resourcesToBeUpdated[i].category}`,
       })
       FIELDS.push({
-        type: 'plain_text',
-        text: resourcesToBeUpdated[i].resourceName
+        type: "plain_text",
+        text: resourcesToBeUpdated[i].resourceName,
       })
     }
     for (let i = 0; i < resourcesToBeDeleted.length; i++) {
       FIELDS.push({
-        type: 'plain_text',
-        text: `[deleted] ${resourcesToBeDeleted[i].category}`
+        type: "plain_text",
+        text: `[deleted] ${resourcesToBeDeleted[i].category}`,
       })
       FIELDS.push({
-        type: 'plain_text',
-        text: resourcesToBeDeleted[i].resourceName
+        type: "plain_text",
+        text: resourcesToBeDeleted[i].resourceName,
       })
     }
-    const statusPath = path.join(__dirname, '..', 'log', 'status.json')
+    const statusPath = path.join(__dirname, "..", "log", "status.json")
     await asyncFSWriteFile(statusPath, JSON.stringify(FIELDS))
   } catch (err) {
     console.error(err)
@@ -50,5 +50,5 @@ async function run(context, args) {
 }
 
 module.exports = {
-  run
+  run,
 }
